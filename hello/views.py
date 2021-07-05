acf318e823814de2b44bf2c70ec65ee2e0cca698
pectoraux <ekontetevi@gmail.com>
2021-07-05 18:57:09 +0000

    mmm

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .forms import DocumentForm
 from .models import Greeting
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = DocumentForm()
+    return render(request, 'index.html', {
+        'form': form
+    })
 
 
 def db(request):
