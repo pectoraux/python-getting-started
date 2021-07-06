0154571647932b7876d7ffca43fbb9ce9cbf1c9d
pectoraux <ekontetevi@gmail.com>
2021-07-06 00:22:17 +0000

    bis

--- hello/admin.py
+++ hello/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from .models import Document
-# Register your models here.
-from attachments.admin import AttachmentInlines
-
-class MyEntryOptions(admin.ModelAdmin):
-    inlines = (AttachmentInlines,)
+# Register your models here. 
     
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
